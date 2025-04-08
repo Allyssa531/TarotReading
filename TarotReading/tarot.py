2e63e6dc4b0c0f97fd3f98325145c99dfdae6a53
magaw001 <magaw001@louisville.edu>
2025-04-08 17:05:19 -0400

    Updated file path.

--- TarotReading/tarot.py
+++ TarotReading/tarot.py
@@ -1,11 +1,11 @@
 from random import randint
 import json
 
-
+filepath = "TarotReading\\decks\\tarot-rw.json"
 
 
 print("Pick a card...")
-with open("tarot-rw.json", "r", encoding="utf-8") as fp:
+with open(filepath, "r", encoding="utf-8") as fp:
     cards = json.load(fp)
 num_of_cards = len(cards)
 card_num = randint(0, num_of_cards)
