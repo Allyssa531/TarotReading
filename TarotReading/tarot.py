01c17fc7362d4ee0a25714d377b9e04f41ab4105
magaw001 <magaw001@louisville.edu>
2025-04-09 11:54:18 -0400

    Added code to tarot.py to display rw cards.

--- TarotReading/tarot.py
+++ TarotReading/tarot.py
@@ -12,3 +12,16 @@ card_num = randint(0, num_of_cards)
 card = cards[card_num]
 
 print(f"Name: {card["name"]}")
+print("Qualities: ")
+print(f"General: {card["qualities"]["general"]}")
+print(f"Upright: {card["qualities"]["upright"]}")
+print(f"Reversed: {card["qualities"]["reversed"]}")
+print("Astrology: ")
+for item in card["astrology"]:
+    print(item)
+print("Elements: ")
+for item in card["elements"]:
+    print(item)
+print(f"Answer: {card["answer"]}")
+
+
